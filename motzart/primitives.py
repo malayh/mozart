c8c1216853c8b0452180addc3ff225f67ea57e87
Malay Hazarika <malay.hazarika@goswift.in>
2022-12-02 10:27:37 +0530

    Adding chord extension and running black

--- motzart/primitives.py
+++ motzart/primitives.py
@@ -1,10 +1,11 @@
-
 from __future__ import annotations
 
 from dataclasses import dataclass
 from enum import Enum
+import re
 
-MAJOR_SCALE_INTERVALS = [2,2,1,2,2,2,1]
+MAJOR_SCALE_INTERVALS = [2, 2, 1, 2, 2, 2, 1]
+
 
 class Mode(Enum):
     ionian = 0
@@ -13,93 +14,100 @@ class Mode(Enum):
     lydian = 3
     mixolydian = 4
     aeolian = 5
-    locarian  = 6
+    locarian = 6
 
-
-    def get_scale(self,root: MidiNote) -> Scale:
+    def get_scale(self, root: MidiNote) -> Scale:
         offset = self.value
         notes = [root]
         _last_note = root
 
         for i in range(7):
-            interval = MAJOR_SCALE_INTERVALS[(i+offset) % 7]
+            interval = MAJOR_SCALE_INTERVALS[(i + offset) % 7]
             _last_note = _last_note.next_by_interval(interval)
             notes.append(_last_note)
 
-        return Scale(notes,self)
+        return Scale(notes, self)
+
 
 class Note(Enum):
     # value is base midi note value
     C = 0
     C_sharp = 1
-    D = 2 
+    D = 2
     D_sharp = 3
     E = 4
     F = 5
-    F_sharp = 6 
+    F_sharp = 6
     G = 7
     G_sharp = 8
     A = 9
-    A_sharp = 10 
+    A_sharp = 10
     B = 11
 
     def get_midi(self, octave: int):
-        return MidiNote(note=self,octave=octave)
+        return MidiNote(note=self, octave=octave)
+
 
 @dataclass
 class MidiNote:
-    note : Note
+    note: Note
     octave: int = 0
 
     @property
     def midi(self) -> int:
-        return self.note.value + (12*self.octave)
+        return self.note.value + (12 * self.octave)
 
-    def next_by_interval(self, interval :int):
-        next_note = Note((self.note.value+interval )%12)
-        next_note_octave = self.octave + ((self.note.value+interval )//12)
+    def next_by_interval(self, interval: int):
+        next_note = Note((self.note.value + interval) % 12)
+        next_note_octave = self.octave + ((self.note.value + interval) // 12)
 
-        return MidiNote(next_note,next_note_octave)
- 
+        return MidiNote(next_note, next_note_octave)
+
     def sharpen(self, semitone: int = 1):
         # TODO: Add boundry checks
-        self.octave += (self.note.value+semitone)//12
-        self.note = Note((self.note.value+semitone)%12)
+        self.octave += (self.note.value + semitone) // 12
+        self.note = Note((self.note.value + semitone) % 12)
         return self
 
     def flatten(self, semitone: int = 1):
         # TODO: Add boundry checks
-        self.octave = self.octave + (self.note.value-semitone) // 12
-        self.note = Note((self.note.value-semitone)%12)
+        self.octave = self.octave + (self.note.value - semitone) // 12
+        self.note = Note((self.note.value - semitone) % 12)
         return self
 
-
     def duplicate(self) -> MidiNote:
-        return MidiNote(self.note,self.octave)
+        return MidiNote(self.note, self.octave)
+
 
 @dataclass
 class Scale:
     # Assume scale has 8 notes.
-    notes : list[MidiNote]
+    notes: list[MidiNote]
     mode: Mode
 
+    @property
+    def root(self):
+        return self.notes[0].note
 
-    def get_note_by_distance(self,start: int, distance: int) -> MidiNote:
+    def get_note_by_distance(self, start: int, distance: int) -> MidiNote:
+        """
+        `start` is index of the starting note in self.notes
+        """
         position = start + distance
 
-        note = self.notes[position%7]
+        note = self.notes[position % 7]
         if position // 7 > 0:
             note = note.duplicate()
             note.octave += position // 7
 
         return note
 
-    def get_note_by_degree(self, degree: int ) -> MidiNote:
+    def get_note_by_degree(self, degree: int) -> MidiNote:
         # Assume scale has 8 notes.
         # Degree starts from 1
 
         position = degree - 1
-        note = self.notes[position%7]
+        note = self.notes[position % 7]
 
         if position // 7 > 0:
             note = note.duplicate()
@@ -107,24 +115,55 @@ class Scale:
 
         return note
 
-    def get_diatonic_chord(self,degree: int):
+    def get_diatonic_triad(self, degree: int):
         # degree starts at 1
         notes = [
-            self.get_note_by_distance(degree-1,0),
-            self.get_note_by_distance(degree-1,2),
-            self.get_note_by_distance(degree-1,4),
+            self.get_note_by_distance(degree - 1, 0),
+            self.get_note_by_distance(degree - 1, 2),
+            self.get_note_by_distance(degree - 1, 4),
         ]
-        return Chord(notes,self,degree-1)
+        return Chord(notes, self, degree)
+
 
 @dataclass
 class Chord:
-    notes : list[MidiNote]
-    scale : Scale
-    position : int 
+    notes: list[MidiNote]
+    scale: Scale
+    degree: int
 
-    def extend(self,notes:list[MidiNote]):
-        self.notes.extend(notes)
+    def extend(self, degree: int, argumentation: str | None = None):
+        """
+        `argumentation` : Can be one of `sharp`, `flat` or None
+        """
+
+        note = self.scale.get_note_by_distance(self.degree - 1, degree - 1)
+        if argumentation:
+            note = note.duplicate()
+            if argumentation == "flat":
+                note.flatten()
+            else:
+                note.sharpen()
+
+        self.notes.append(note)
+        return self
+
+    def extend_many(self, degrees: list[str]):
+        """
+        `degrees` : list of string containing scale degrees as follows `<b#><degree>`. e.g. : "#4", "b3" etc
+        """
+        _expr = re.compile(r"^([b#])?(\d{1,2})$")
+        for d in degrees:
+            match = re.match(_expr, d)
+            if not match:
+                continue
+
+            arg = match.group(1)
+            if arg:
+                arg = "flat" if arg == "b" else "sharp"
+
+            degree = int(match.group(2))
+            self.extend(degree, argumentation=arg)
+
+        return self
 
 
-if __name__ == "__main__":
-    pass
