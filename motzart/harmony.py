c55f728a69b03847847812401e03c6bcd3e29ae1
Malay Hazarika <malay.hazarika@goswift.in>
2022-12-04 21:56:54 +0530

    generate v2

--- motzart/harmony.py
+++ motzart/harmony.py
@@ -36,13 +36,21 @@ class Resolution:
 
 
 class Cadence:
+    # TODO: MINOR_PLAGAL : Cannot encode this, because can't mention `minor 4th` as start from
+
     AUTHENTIC = Resolution(5, [5], [1])
     PLAGAL = Resolution(4, [4], [1])
     HALF = Resolution(2, [1, 2, 3, 4, 5, 6, 7], [5])
     DECEPTIVE = Resolution(
         1, ChordCategory.DOMINENT.value, ChordCategory.SUB_DOMINENT.value
     )
-    # MINOR_PLAGAL : TODO : Cannot encode this, because can't mention `minor 4th` as start from
+
+    # Not a cadence
+    SUB_TO_DOM = Resolution(
+        0,
+        ChordCategory.SUB_DOMINENT.value,
+        ChordCategory.DOMINENT.value,
+    )
 
     # Not a cadence
     TONIC_TO_OTHER = Resolution(
@@ -55,13 +63,6 @@ class Cadence:
         0,
         ChordCategory.SUB_DOMINENT.value,
         ChordCategory.SUB_DOMINENT.value,
-    )
-
-    # Not a cadence
-    SUB_TO_DOM = Resolution(
-        1,
-        ChordCategory.SUB_DOMINENT.value,
-        ChordCategory.DOMINENT.value,
     )
 
     all_cadences: list[Resolution] = [
@@ -105,8 +106,22 @@ class Cadence:
 @dataclass
 class ChordProgression:
     key: Note
-    mode: Mode
+    scale: Scale
     chords: list[Chord]
+
+    def add_spice(self, level: int = 1):
+        spice = ["7"]
+        if level == 2:
+            spice.append("9")
+
+        if level == 3:
+            spice.append("11")
+
+        if level > 3:
+            spice.append("13")
+
+        for chord in self.chords:
+            chord.extend_many(spice)
 
 
 class ChordProgressionGenerator:
@@ -114,96 +129,63 @@ class ChordProgressionGenerator:
     octave: int = 4
     scale: Scale
     mode: Mode
-
-    lenght = 4
-    chords: list[Chord] = []
+    lenght: int = 4
 
     def __init__(self, key: Note, mode: Mode, octave: int = 4, lenght: int = 4) -> None:
         self.key = key
         self.mode = mode
         self.octave = octave
         self.scale = self.mode.get_scale(self.key.get_midi(self.octave))
-        self.lenght = lenght
-
-    def chooce_final_chords(self) -> tuple[Chord, Chord]:
-        cadence = random.choice([Cadence.AUTHENTIC, Cadence.PLAGAL])
-        if self.lenght >= 6:
-            cadence = Cadence.AUTHENTIC
-
-        final = self.scale.get_diatonic_triad(1)
-
-        _rr = Cadence.reverse_resolve(final, cadence.strenght)
-        pre_final = self.scale.get_diatonic_triad(_rr)
-
-        return pre_final, final
-
-    def spice_up(self, progession: ChordProgression) -> ChordProgression:
-        spice = ["9", "11"]
-
-        add_7th = random.random() < 0.6
-        if add_7th:
-            for chord in progession.chords:
-                if random.random() < 0.9:
-                    chord.extend(7)
-
-        #  20% chance of adding abit more spice to the chord
-        add_more_spice = random.random() < 0.2
-        if add_more_spice:
-            for chord in progession.chords:
-                if random.random() < 0.9:
-                    chord.extend_many(random.choices(spice, k=1))
-
-        return progession
+        self.lenght = lenght + (lenght % 2)
 
     def generate(self) -> ChordProgression:
-        """
-        - v1 feats
-            - always resolves to tonic
-            - always starts with tonic
-            - randomize adding 7ths
-            - randomize adding other extensions
-            - randomize replacing diminished
-        """
+        return self.generate_v2()
 
-        pre_final, final = self.chooce_final_chords()
+    def _resolve_forward(self, chord: Chord, strenght_choices: list[int]) -> Chord:
+        strength = random.choice(strenght_choices)
+        degree = Cadence.resolve(chord, strength)
+        while degree == -1:
+            strength = random.choice(strenght_choices)
+            degree = Cadence.resolve(chord, strength)
 
-        chords = [None] * (self.lenght - 2)
-        chords.extend([pre_final, final])
+        if degree == -1:
+            raise ValueError("Can not resolve chord")
 
-        chords[0] = self.scale.get_diatonic_triad(1)
+        return self.scale.get_diatonic_triad(degree)
 
-        for i in range(1, self.lenght - 2):
-            chords[i] = self.scale.get_diatonic_triad(
-                random.choice([1, 2, 3, 4, 5, 6, 7])
-            )
-
-            # If currently selected chord is dimiished, then there is 50% chance that it would be tried to be replaced 3 times
-            if chords[i].chord_type == ChordType.diminished and random.random() < 0.5:
-                for _ in range(2):
-                    chords[i] = self.scale.get_diatonic_triad(
-                        random.choice([1, 2, 3, 4, 5, 6, 7])
-                    )
-                    if chords[i].chord_type != ChordType.diminished:
-                        break
-
-            # Try 3 times to get a different chord than the previous one
-            if i > 0 and chords[i] == chords[i - 1]:
-                for _ in range(3):
-                    chords[i] = self.scale.get_diatonic_triad(
-                        random.choice([1, 2, 3, 4, 5, 6, 7])
-                    )
-                    if chords[i] != chords[i - 1]:
-                        break
-
-        progession = ChordProgression(self.key, self.mode, chords)
-        return self.spice_up(progession)
-
-    def generate_v2(self):
+    def generate_v2(self, resolution_strenght: int = 5):
         """
         - v2 feats
             - minimum lenght of 2
             - renders 2 chord at a time, each pair resolves to some extent
-            - Progressions does not have to start or end at tonic
+            - * NOT doing this *: Progressions does not have to start or end at tonic
             - Progressions can end at 5th if there is a follow up section in the code
         """
-        pass
+
+        final_cadence = Cadence.AUTHENTIC
+        if resolution_strenght == 4:
+            final_cadence = Cadence.PLAGAL
+        elif resolution_strenght == 2:
+            final_cadence = Cadence.HALF
+        elif resolution_strenght == 1:
+            final_cadence = Cadence.DECEPTIVE
+
+        chords: list[Chord] = [self.scale.get_diatonic_triad(1)]
+        strenght_choices = [0, 1, 2]
+
+        for i in range(1, self.lenght - 2):
+            chord = self._resolve_forward(chords[i - 1], strenght_choices)
+
+            while chord == chords[i - 1]:
+                chord = self._resolve_forward(chords[i - 1], strenght_choices)
+
+            chords.append(chord)
+
+        chords.append(
+            self.scale.get_diatonic_triad(random.choice(final_cadence.starts_from))
+        )
+        chords.append(
+            self.scale.get_diatonic_triad(random.choice(final_cadence.resolves_to))
+        )
+
+        return ChordProgression(self.key, self.scale, chords)
--- tests/try_harmony.py
+++ tests/try_harmony.py
@@ -3,26 +3,16 @@ import script_include
 from dev_utils import play_chord
 from motzart.primitives import Chord, Mode, Note, MidiNote
 from motzart.harmony import ChordProgression, ChordProgressionGenerator
-import time
 
 
-def try_chord_generator():
-
-    while True:
-        cpg = ChordProgressionGenerator(Note.E, Mode.lydian, octave=3, lenght=6)
-        progression = cpg.generate()
-        for chord in progression.chords:
-            play_chord(chord, arpeggiate=True)
-
-    # for mode in Mode:
-    #     cpg = ChordProgressionGenerator(Note.C,mode)
-    #     progression = cpg.generate_v1()
-
-    #     for chord in progression.chords:
-    #         play_chord(chord)
-
-    #     time.sleep(1)
+def try_generator_v2():
+    cpg = ChordProgressionGenerator(Note.E, Mode.lydian, octave=4, lenght=6)
+    progression = cpg.generate_v2(resolution_strenght=5)
+    progression.add_spice(level=1)
+    for chord in progression.chords:
+        play_chord(chord, arpeggiate=True)
 
 
 if __name__ == "__main__":
-    try_chord_generator()
+    # try_chord_generator()
+    try_generator_v2()
