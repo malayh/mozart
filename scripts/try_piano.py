0b0128d4d5cfd56dc181ff411c2ad99ab11e11be
Malay Hazarika <malay.hazarika@goswift.in>
2022-12-12 22:31:14 +0530

    Moving try scripts

--- scripts/try_piano.py
+++ scripts/try_piano.py
@@ -9,18 +9,21 @@ from motzart.primitives import Mode, Note, TimeSignature
 def dummy_play_progession():
     # random.seed(100)
 
-    generator = ChordProgressionGenerator(Note.C, Mode.lydian, lenght=6, octave=4)
+    generator = ChordProgressionGenerator(Note.C, Mode.ionian, lenght=4, octave=4)
     progression = generator.generate_v2(resolution_strenght=5, start_with=ChordCategory.TONIC)
     player = Player(bpm=120)
 
     clip = Clip()
-    art = PianoChordArticulator(time_signature=TimeSignature(7, 4), intensity=4)
+    art = PianoChordArticulator(time_signature=TimeSignature(4, 4), intensity=4)
     for i in range(len(progression.chords)):
         chord = progression.chords[i]
         chord.extend_many(["7", "9"])
         clip.concat(art.articulate_arp_with_bass(chord, sloppyness=0))
 
     print(clip.starts_at, clip.ends_at)
+    for c in clip.played_notes:
+        c.midi_channel = 0
+
     player.render(clip.played_notes)
     player.play()
 
